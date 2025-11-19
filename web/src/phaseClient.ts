be5ba876aedacab961f1d54287d0fd1f4fb84d95
seb suarez <162302050+cbassuarez@users.noreply.github.com>
2025-11-19 00:11:27 -0800

    Update phaseClient.ts

--- web/src/phaseClient.ts
+++ web/src/phaseClient.ts
@@ -1,15 +1,7 @@
 // web/src/phaseClient.ts
-<<<<<<< HEAD
 import init, { WasmEngine } from "./wasm/phasewasm";
 
 export async function loadWasmEngine() {
-  // Initialize the WASM module
-=======
-
-import init, { WasmEngine } from "./wasm/phasewasm";
-
-export async function loadWasmEngine() {
->>>>>>> b624fef (add web docs)
   await init();
   const engine = new WasmEngine();
   return { engine };
